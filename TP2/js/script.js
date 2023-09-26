b3356f1b1b045d8a461523fdac16a133a0af8ab1
Valentin <valentincaminos12345@gmail.com>
2023-09-25 21:47:45 -0300

    login casi listo, falta redimencionar el fondo

---
+++ TP2/js/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+let btnIniciaSesion = document.querySelector(".btn_inicia_sesion").addEventListener("click", showLogin);
+
+function showLogin() {
+
+    // Oculto los input
+    let contInputs = [
+        document.querySelector(".form_name"),
+        document.querySelector(".form_apellido"),
+        document.querySelector(".form_username"),
+        document.querySelector(".form_edad"),
+        document.querySelector(".form_mail"),
+        document.querySelector(".form_password"),
+        document.querySelector(".form_passwordRep"),
+        document.querySelector(".captcha")
+    ];
+
+    contInputs.forEach(element => {
+        element.classList.toggle("dis_none");
+    });
+
+    // Aparecen los botones de login
+    let contInputLogin = document.querySelectorAll(".cont_input_login");
+    console.log(contInputLogin);
+
+    contInputLogin.forEach(element => {
+        element.classList.toggle("showLogin");
+    });
+
+    document.querySelector(".cont_linea").classList.toggle("showLinea");
+
+    // Le cambio el texto al boton
+    document.querySelector(".btn_registrarse").textContent = "Iniciar sesion"
+
+    //Le cambio el tamaño al fondo
+    document.querySelector(".content_imgForm").classList.toggle("content_imgForm_login");
+}
