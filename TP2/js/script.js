c1d16d6ad86d0a38990ce3c22ac7b77b727bd4d1
Valentin <valentincaminos12345@gmail.com>
2023-09-30 22:51:47 -0300

    fix bug en desplegables y imagenes de carrouseles

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -3,27 +3,31 @@
 // Desplegable categorias
 const slidebar_burguer = document.querySelector(".sidebar_burguer");
 const slidebar_perfil = document.querySelector(".sidebar_perfil");
+const overlay = document.querySelector(".overlay");
 
 document.querySelector(".cont_burguer").addEventListener("click", showBurguer);
 document.querySelector(".btn_cerrar_burguer").addEventListener("click", hiddenBuguer);
-
 function showBurguer() {
     slidebar_burguer.classList.remove("noVisible_burguer");
     slidebar_burguer.classList.add("visible_burguer");
+    overlay.style.display = 'block';
 }
 
 function hiddenBuguer() {
     slidebar_burguer.classList.add("noVisible_burguer");
+    overlay.style.display = 'none';
 }
 
-document.querySelector(".cont_perfil_img").addEventListener("click", showMenuPerfil);
-document.querySelector(".btn_cerrar_perfil").addEventListener("click", hiddenMenuPerfil);
+const btnAbrirMenu = document.querySelector(".cont_perfil_img");
+btnAbrirMenu.addEventListener("click", () => {
+    slidebar_perfil.style.width = "293px";
+    overlay.style.display = 'block';
 
-function showMenuPerfil() {
-    slidebar_perfil.classList.remove("noVisible_perfil");
+});
 
-    slidebar_perfil.classList.add("visible_perfil");
-}
-function hiddenMenuPerfil() {
-    slidebar_perfil.classList.add("noVisible_perfil");
-}
+// Agrega un evento clic al botón de cerrar el menú
+document.querySelector(".btn_cerrar_perfil").addEventListener("click", () => {
+    slidebar_perfil.style.width = "0";
+    overlay.style.display = 'none';
+
+});
