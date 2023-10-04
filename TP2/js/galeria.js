89b6a9c9cc3ce10bbe3729d4a2fb13551c708dc2
Valentin <valentincaminos12345@gmail.com>
2023-10-03 22:29:56 -0300

    se termino la galeria de imagenes?

--- TP2/js/galeria.js
+++ TP2/js/galeria.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// Scroll de la galera
+// Scroll de la galeria
 let btn_right = document.querySelector(".cont_btn_carrousel_right");
 let btn_left = document.querySelector(".cont_btn_carrousel_left");
 
@@ -19,34 +19,21 @@ btn_left.addEventListener("click", () => {
 
 let imagenSeleccionada = document.getElementById("imagenSeleccionada");
 let imagenes = document.querySelectorAll(".cont_img_car img");
-let cont = document.querySelectorAll(".contenedor_img_principal");
+let contenedorImgPrincipal = document.querySelector(".contenedor_img_principal");
 
 imagenes.forEach(function (imagen) {
 
 
-    imagen.addEventListener("click", function () {
+    imagen.addEventListener("click", () => {
 
-
-        
-        // setTimeout(function () {
-        //     imagenSeleccionada.style.opacity = "0";
-        // }, 300);
-
-        //   setTimeout(function () {
-        //     imagenSeleccionada.style.opacity = "1";
-        //     imagenSeleccionada.style.animation = "mostrarImagen 2s linear 0 1 normal none";
-        //     imagenSeleccionada.src = imagen.src;
-        // }, 550);
-
-        setTimeout(function () {
-            imagenSeleccionada.style.transition = "opacity 0.3s ease-in-out";
-            imagenSeleccionada.style.opacity = "0";
-        }, 300);
-
-        setTimeout(function () {
-            imagenSeleccionada.style.opacity = "1";
+        const imagenClonada = imagen.cloneNode();
+        imagenClonada.style.animation = "animacionGaleria 0.4s ease-in forwards";
+        //Evitamos que se duplique
+        contenedorImgPrincipal.innerHTML = "";
+        contenedorImgPrincipal.appendChild(imagenClonada);
+        setTimeout(() => {
             imagenSeleccionada.src = imagen.src;
-        }, 550);
-
+            imagenClonada.style.animation = "";
+        }, 400);
     })
 });
--- TP2/js/script.js
+++ TP2/js/script.js
@@ -36,16 +36,16 @@ document.querySelector(".btn_cerrar_perfil").addEventListener("click", () => {
 "use strict"
 
 // Scroll de la galera
-let btn_right = document.querySelector(".cont_btn_carrousel_right");
-let btn_left = document.querySelector(".cont_btn_carrousel_left");
+// let btn_right = document.querySelector(".cont_btn_carrousel_right");
+// let btn_left = document.querySelector(".cont_btn_carrousel_left");
 
-let carrouselLi_container = document.querySelector(".carrouselLi_container");
-const scrollAmount = 200;
+// let carrouselLi_container = document.querySelector(".carrouselLi_container");
+// const scrollAmount = 200;
 
-btn_right.addEventListener("click", () => {
-    carrouselLi_container.scrollLeft += scrollAmount;
-});
+// btn_right.addEventListener("click", () => {
+//     carrouselLi_container.scrollLeft += scrollAmount;
+// });
 
-btn_left.addEventListener("click", () => {
-    carrouselLi_container.scrollLeft -= scrollAmount;
-});
+// btn_left.addEventListener("click", () => {
+//     carrouselLi_container.scrollLeft -= scrollAmount;
+// });
