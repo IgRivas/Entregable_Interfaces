698a089be37c8d41fa1991813ebe48a396062473
Valentin <valentincaminos12345@gmail.com>
2023-10-04 22:00:00 -0300

    footer listo

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -63,13 +63,7 @@ let btn_legal_footer = document.getElementById("btn_legal_footer");
 let cont_legar_footer = document.querySelector(".cont_legal_footer");
 
 btn_categorias_footer.addEventListener("click", () => {
-    
-    cont_categorias.style.animation = "animacionFooter 0.3 ease-in infinite";
-    
-    setTimeout(()=>{
-        cont_categorias.classList.toggle("con_categorias_visible");
-        cont_categorias.style.animation ="";
-    },400)
+    cont_categorias.classList.toggle("con_categorias_visible");
 });
 
 
