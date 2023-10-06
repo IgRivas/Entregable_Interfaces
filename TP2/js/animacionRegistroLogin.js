58f441d71ff3db8f1ecf878f0b2d0c52a7dc59fd
Valentin <valentincaminos12345@gmail.com>
2023-10-06 15:45:45 -0300

    carruseles genericos terminados, refactor js y arreglo de bugs

--- TP2/js/animacionRegistroLogin.js
+++ TP2/js/animacionRegistroLogin.js
@@ -1,36 +1,35 @@
 "use strict"
 
-let cont_animacion_registro = document.querySelector(".cont_animacion_registro");
-let overlayFormulario = document.querySelector(".overlayFormulario");
-document.getElementById("btn_form").addEventListener("click", () => {
-    cont_animacion_registro.classList.toggle("quitar");
-    overlayFormulario.style.display = "block";
-
-    setTimeout(function () {
-        window.location.href = "./home.html";
-    }, 3000);
-
-});
-
+if(document.querySelector(".cont_animacion_registro") && document.querySelector(".overlayFormulario")){
+    let cont_animacion_registro = document.querySelector(".cont_animacion_registro");
+    let overlayFormulario = document.querySelector(".overlayFormulario");
+    document.getElementById("btn_form").addEventListener("click", () => {
+        cont_animacion_registro.classList.toggle("quitar");
+        overlayFormulario.style.display = "block";
+    
+        setTimeout(function () {
+            window.location.href = "./home.html";
+        }, 3000);
+    
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-    function updateCounter() {
-        const counterElement = document.getElementById("cont");
+    function actualizarCont() {
+        const cont = document.getElementById("cont");
         let count = 0;
-        const interval = 50;
-        const counterInterval = setInterval(function () {
+        const intervalo = 50;
+        const contadorIntervalo = setInterval(function () {
             if (count == 100) {
-                clearInterval(counterInterval);
-
-
+                clearInterval(contadorIntervalo);
             } else {
                 count++;
-                counterElement.innerHTML = count + "%";
+                cont.innerHTML = count + "%";
             }
-        }, interval);
+        }, intervalo);
     }
 
-    updateCounter();
+    actualizarCont();
 
     let overlayAnimacionLoader = document.querySelector(".section_loader");
 
--- TP2/js/galeria.js
+++ TP2/js/galeria.js
@@ -1,60 +1,65 @@
 "use strict"
 
-// Scroll de la galeria
-const cont_img_car = document.querySelector(".cont_img_car");
-const tamanioScroll = 645;
-let maxScroll = cont_img_car.scrollWidth + cont_img_car.clientWidth;
+// carrusel generico
+let carruseles = document.querySelectorAll(".carrusel_generico");
+const scrollCarruselJuegos = 1000;
 
-// scroll y cambio de color right
-let btn_right = document.querySelector(".cont_btn_carrousel_right")
-btn_right.addEventListener("click", () => {
-    cont_img_car.scrollLeft += tamanioScroll;
-    verificarFinCarrusel();
-});
-cont_img_car.addEventListener("scroll", verificarFinCarrusel);
-function verificarFinCarrusel() {
-    if (cont_img_car.scrollLeft + cont_img_car.clientWidth >= cont_img_car.scrollWidth) {
-        btn_right.style.backgroundColor = "#2E90CC";
-    } else {
-        btn_right.style.backgroundColor = "#3FBFE9";
-    }
-}
-
-// scroll y cambio de color left
-let btn_left = document.querySelector(".cont_btn_carrousel_left");
-btn_left.addEventListener("click", () => {
-    cont_img_car.scrollLeft -= tamanioScroll;
-});
-cont_img_car.addEventListener("scroll", verificarInicioCarrusel);
-function verificarInicioCarrusel() {
-    if (cont_img_car.scrollLeft === 0) {
-        btn_left.style.backgroundColor = "#2E90CC";
-
-    } else {
-        btn_left.style.backgroundColor = "#3FBFE9";
-
-    }
-}
-
-// Intercambio de imagenes
-
-let imagenSeleccionada = document.getElementById("imagenSeleccionada");
-let imagenes = document.querySelectorAll(".cont_img_car img");
-let contenedorImgPrincipal = document.querySelector(".contenedor_img_principal");
-
-imagenes.forEach(function (imagen) {
-    imagen.addEventListener("click", () => {
-        const imagenClonada = imagen.cloneNode();
-        imagenClonada.style.animation = "animacionGaleria 0.4s ease-in forwards";
-        //Evitamos que se duplique
-        contenedorImgPrincipal.innerHTML = "";
-        contenedorImgPrincipal.appendChild(imagenClonada);
-        setTimeout(() => {
-            imagenSeleccionada.src = imagen.src;
-            imagenClonada.style.animation = "";
-        }, 400);
+let btn_carruseles_right = document.querySelectorAll(".btn_carrusel_right");
+btn_carruseles_right.forEach((boton,index)=>{
+    boton.addEventListener("click", ()=>{
+        let carrusel = carruseles[index];
+        carrusel.scrollLeft += scrollCarruselJuegos;
+        carrusel.addEventListener("scroll", verificarFinCarrusel);
+        function verificarFinCarrusel() {
+            if (carrusel.scrollLeft + carrusel.clientWidth >= carrusel.scrollWidth) {
+                boton.style.backgroundColor = "#2E90CC";
+            } else {
+                boton.style.backgroundColor = "#3FBFE9";
+            }
+        }
     })
 });
+
+let btn_carruseles_left = document.querySelectorAll(".btn_carrusel_left");
+btn_carruseles_left.forEach((boton,index)=>{
+    let carrusel = carruseles[index];
+    carrusel.addEventListener("scroll", verificarInicioCarrusel);
+    function verificarInicioCarrusel() {
+        if (carrusel.scrollLeft === 0) {
+            boton.style.backgroundColor = "#2E90CC";
+
+        } else {
+            boton.style.backgroundColor = "#3FBFE9";
+
+        }
+    }
+    boton.addEventListener("click", ()=>{
+        carrusel.scrollLeft -= scrollCarruselJuegos;
+    })
+});
+
+
+// Intercambio de imagenes
+if((document.getElementById("imagenSeleccionada")) && (document.querySelectorAll(".cont_img_car img"))){
+    let imagenSeleccionada = document.getElementById("imagenSeleccionada");
+    let imagenes = document.querySelectorAll(".cont_img_car img");
+    let contenedorImgPrincipal = document.querySelector(".contenedor_img_principal");
+    imagenes.forEach(function (imagen) {
+        imagen.addEventListener("click", () => {
+            const imagenClonada = imagen.cloneNode();
+            imagenClonada.style.animation = "animacionGaleria 0.4s ease-in forwards";
+            //Evitamos que se duplique
+            contenedorImgPrincipal.innerHTML = "";
+            contenedorImgPrincipal.appendChild(imagenClonada);
+            setTimeout(() => {
+                imagenSeleccionada.src = imagen.src;
+                imagenClonada.style.animation = "";
+            }, 400);
+        })
+    });
+    
+}
+
 
 
 
